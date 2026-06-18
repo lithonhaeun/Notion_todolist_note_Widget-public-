3c7fee1a9e377fae9ba6c8d9ae129cf9c51578d7
lithonhaeun <165010789+lithonhaeun@users.noreply.github.com>
2026-06-19 00:15:48 +0900

    Update next.config.js
    웹임베드 가능하게 수정

--- next.config.js
+++ next.config.js
@@ -3,12 +3,11 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // 노션 안에서 iframe 임베드를 허용하기 위한 헤더
         source: '/:path*',
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: 'frame-ancestors https://*.notion.so https://*.notion.site',
+            value: 'frame-ancestors https://*.notion.so https://*.notion.site https://*.notion.com https://www.notion.so;',
           },
         ],
       },
